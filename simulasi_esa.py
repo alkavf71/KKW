5f6099608e951187e18d6b2d88ca2b337e8c3e44
Muhammad Al Kahfi <76249845+alkavf71@users.noreply.github.com>
2026-02-01 15:41:25 +0800

    Update simulasi_esa.py

--- simulasi_esa.py
+++ simulasi_esa.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Reliability Assistant Pro",
     layout="wide",
-    page_icon="🛡️",
+    page_icon="🛡️🙏🏻",
     initial_sidebar_state="expanded"
 )
 
